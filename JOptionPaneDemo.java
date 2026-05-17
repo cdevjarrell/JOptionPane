b46dae38b1ca956408a0cfa8e5fb8942e2592f8a
Christian Jarrell <christian.j.jarrell@gmail.com>
2026-05-17 17:14:05 -0400

    Initial setup: add JOptionPaneDemo class with comment

---
+++ JOptionPaneDemo.java
@@ -0,0 +1,14 @@
+/*
+ * This program uses JOptionPane to prompt the user for a username and password.
+ * It compares the input to stored credentials and displays either
+ * "Access Granted" or "Incorrect Credentials" based on the result.
+ */
+
+import javax.swing.JOptionPane;
+
+public class JOptionPaneDemo {
+
+    public static void main(String[] args) {
+        // Code will be added here
+    }
+}
